c1f50f9819b24ef8dd8a7ec114d9431ff65fb2bb
theo-boi <boisseau.theo@gmail.com>
2022-01-19 22:39:37 +0100

    Fichiers de tests

---
+++ src/test/java/coffeemachine/coffeemachine/IntegrationTests.java
@@ -0,0 +1,41 @@
+package coffeemachine.coffeemachine;
+
+import coffeemachine.coffeemachine.view.MainView;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class IntegrationTests
+        extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public IntegrationTests( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        MainView.main(null); //ligne inutile
+        assertTrue( true );
+        /*List mockedList = mock(List.class); //using mock object
+        mockedList.add("one"); mockedList.clear(); //verification
+        verify(mockedList).add("one");
+        verify(mockedList).clear();*/
+    }
+}
